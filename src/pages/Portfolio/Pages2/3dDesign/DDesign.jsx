e85369eccaab550784af029e1bc417d3d2ba5633
Khyel <khyelcalanuga@gmail.com>
2026-04-05 16:26:33 +0800

    UploadImgs

--- src/pages/Portfolio/Pages2/3dDesign/DDesign.jsx
+++ src/pages/Portfolio/Pages2/3dDesign/DDesign.jsx
@@ -5,6 +5,7 @@ import buttOil1 from '../../../../assets/ProductRendering/1.png'
 import buttOil2 from '../../../../assets/ProductRendering/2.png'
 import buttOil3 from '../../../../assets/ProductRendering/3.png'
 import wireframe from '../../../../assets/ProductRendering/wireframe.png'
+import 
 
 const DDesign = () => {
     const [selectedImg, setSelectedImg] = useState(null);
@@ -20,6 +21,18 @@ const DDesign = () => {
         <div className="contentsGD">
             <div className="gallerFixer">
                 <div className="galleryPoster2">
+                    <p>Product Rendering</p>
+                    <div className="posterGal2">
+                        <img src={bodyScrub} alt="Burger 2" onClick={() => openModal(bodyScrub)} />
+                        <img src={buttOil1} alt="Tempura Poster" onClick={() => openModal(buttOil1)} />
+                        <img src={buttOil2} alt="Royal Poster" onClick={() => openModal(buttOil2)} />
+                    </div>
+                    <div className="posterGal3">
+                        <img src={buttOil3} alt="Burger " onClick={() => openModal(buttOil3)} />
+                        <img src={wireframe} alt="Tempura Poster" onClick={() => openModal(wireframe)} />
+                    </div>
+                </div>
+                <div className="galleryPoster2">
                     <p>Food Poster</p>
                     <div className="posterGal2">
                         <img src={bodyScrub} alt="Burger 2" onClick={() => openModal(bodyScrub)} />
@@ -29,7 +42,6 @@ const DDesign = () => {
                     <div className="posterGal3">
                         <img src={buttOil3} alt="Burger " onClick={() => openModal(buttOil3)} />
                         <img src={wireframe} alt="Tempura Poster" onClick={() => openModal(wireframe)} />
-
                     </div>
                 </div>
             </div>
