826d7c1319068e1108c4353fa2d2303e13f55c57
Khyel <khyelcalanuga@gmail.com>
2026-04-06 05:09:26 +0800

    newChange

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import './Products.css'
 
 const Products = () => {
     return (
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
-            <h1>Products Page</h1>
-            <p>Our products will be showcased here.</p>
+        <div className="products">
+            <h1>Ginagawa pa wait</h1>
         </div>
     )
 }
